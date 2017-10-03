81c046c0dcaaa2007b9e21fde2a10953ac577a20
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-10-04 00:24:25 +0300

    tasks parameters updated

--- home/tasks.py
+++ home/tasks.py
@@ -6,12 +6,12 @@ from .tokens import account_activation_token
 from celery import shared_task
 
 @shared_task
-def send_email(user, user_pk, current_site):
+def send_email(user, current_site):
 	subject = 'Activate Your MySite Account'
 	message = render_to_string('registration/account_activation_email.html', {
 	    'user': user,
 	    'domain': current_site,
-	    'uid': urlsafe_base64_encode(force_bytes(user_pk)),
+	    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
 	    'token': account_activation_token.make_token(user),
 	})
 	user.email_user(subject, message)
