67c85bfeef4f87778a48882ed3fe024d7b2722ee
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-10-04 18:16:32 +0300

    celery task comented

--- home/views.py
+++ home/views.py
@@ -49,7 +49,17 @@ class SignUp(View):
             user.save()
 
             current_site = get_current_site(request)
-            send_email.delay(user=user, current_site=current_site.domain)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            # Celery task
+            # send_email.delay(user=user, current_site=current_site.domain)
 
             return redirect('/')
         return render(request, self.template_name, {'form': form})
